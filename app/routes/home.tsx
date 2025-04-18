75d93f621622093ea48beeb1187e3b61506366d3
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-04-18 22:20:27 +0200

    adjust meta data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "KeepTrack" },
+    { name: "description", content: "All the items you want to track" },
   ];
 }
 
